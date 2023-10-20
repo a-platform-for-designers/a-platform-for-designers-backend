3a04cd0a0e19e161afca698c1b0b9000068672f7
Николай Хван <khvannickolay@gmail.com>
2023-10-20 13:15:14 +0500

    Добавлена модель Резюме в приложении job

--- backend/job/models.py
+++ backend/job/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Resume(models.Model):
+    user = models.ForeignKey(
+        'User',
+        on_delete=models.CASCADE,
+        related_name='resume'
+    )
+    skills = models.ForeignKey('Skill', on_delete=models.SET_NULL, null=True)
+    instruments = models.ForeignKey(
+        'Instrument',
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    about = models.TextField()
+    status = instruments
+
+    class Meta:
+        verbose_name = 'Резюме'
+        verbose_name_plural = 'Резюме'
+
+    def __str__(self):
+        return f'Резюме {self.user.username}'
