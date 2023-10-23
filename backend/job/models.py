72ac116648a004a8a09a27122109d7efc1e6a62a
Николай Хван <khvannickolay@gmail.com>
2023-10-23 18:12:35 +0500

    models resume, specialization, resource and contact

--- backend/job/models.py
+++ backend/job/models.py
@@ -21,3 +21,46 @@ class Resume(models.Model):
 
     def __str__(self):
         return f'Резюме {self.user.username}'
+
+
+class Specialization(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = 'Специализация'
+        verbose_name_plural = 'Специализации'
+
+    def __str__(self):
+        return self.name
+
+
+class Resource(models.Model):
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = 'Ресурс'
+        verbose_name_plural = 'Ресурсы'
+
+    def __str__(self):
+        return self.name
+
+
+class Contact(models.Model):
+    user = models.ForeignKey(
+        'User',
+        on_delete=models.CASCADE,
+        related_name='resume'
+    )
+    resource = models.ForeignKey(
+        Resource,
+        on_delete=models.CASCADE
+    )
+    reference = models.CharField(max_length=50)
+    is_preference =  models.BooleanField()
+
+    class Meta:
+        verbose_name = 'Контакт'
+        verbose_name_plural = 'Контакт'
+
+    def __str__(self):
+        return f'Контакт {self.name}'
