f6f50d2bea7c17eede60a41492e4a7535bd10fc0
Dave-YP <115189185+Dave-YP@users.noreply.github.com>
2023-10-24 17:24:56 +0300

    -commit

--- backend/job/admin.py
+++ backend/job/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Case, Instrument, Skill, Favorite
+
+
+@admin.register(Instrument)
+class InstrumentAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'id',
+    )
+
+
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'id',
+    )
+
+
+@admin.register(Case)
+class CaseAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'title',
+        'status',
+        'sphere',
+        'instruments',
+        'skills',
+        'working_time',
+        'description',
+    )
+    fields = (
+        ('title', 'status', 'working_time',),
+        ('description', 'skills',),
+    )
+    list_editable = ('skills',)
+    search_fields = ('title',)
+    list_filter = ('sphere', 'title', 'skills',)
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Favorite)
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'user',
+        'case',
+    )
