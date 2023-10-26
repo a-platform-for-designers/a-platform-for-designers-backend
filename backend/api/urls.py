db0ea8d2735738d4800edef318212f99af4e85d4
pascal-163 <aleksid92@gmail.com>
2023-10-26 20:03:31 +0700

    Добавлены модели Instrument, Skill, Case, Favorite

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,11 @@
 from api.views.user_views import UserProfileViewSet
+from api.views import CaseViewSet
 from django.urls import include, path
 from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register('users', UserProfileViewSet, basename='user')
+router.register('cases', CaseViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
