0441a5f4f18b13519c80771f37d836880ea9e190
Анна МительковаÐ <221882@mail.ru>
2023-10-26 15:36:15 +0300

    basic request api/caseimages with get and post works

--- backend/api/views_common.py
+++ backend/api/views_common.py
@@ -3,5 +3,6 @@ from job.models import CaseImage
 from .serializers_common import CaseImageSerializer
 
 class CaseImageViewSet(viewsets.ModelViewSet):
+    http_method_names = ('get', 'post')
     queryset = CaseImage.objects.all()
     serializer_class = CaseImageSerializer
