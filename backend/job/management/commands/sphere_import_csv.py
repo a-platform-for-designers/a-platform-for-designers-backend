e456c0478a5b0186e819cb1fd375e3f1ca9af8d1
pascal-163 <aleksid92@gmail.com>
2023-11-08 23:39:16 +0700

    Add import db from csv

---
+++ backend/job/management/commands/sphere_import_csv.py
@@ -0,0 +1,15 @@
+import csv
+from django.core.management.base import BaseCommand
+from job.models import Sphere
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        with open('job/management/commands/tools.csv', 'r', encoding='utf-8') as f:
+            csvreader = csv.reader(f)
+            next(csvreader)
+            for row in csvreader:
+                name = row[0]
+                Sphere.objects.create(name=name)
+                self.stdout.write(self.style.SUCCESS(f'Skill "{name}" created.'))
