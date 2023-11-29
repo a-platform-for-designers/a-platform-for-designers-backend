6180df79c3a737d05151a2d4cb2f5cba9cd3203e
pascal-163 <aleksid92@gmail.com>
2023-11-29 23:09:04 +0700

    User date joined

--- backend/users/models.py
+++ backend/users/models.py
@@ -111,7 +111,7 @@ class ProfileDesigner(models.Model):
     language = models.ManyToManyField(
         to='job.Language',
         blank=True,
-        null=True,
+        # null=True,
     )
 
     class Meta:
