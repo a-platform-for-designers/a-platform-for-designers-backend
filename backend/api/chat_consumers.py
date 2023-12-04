7b18ba1a5e90d86c2e46e28ca142e3c8c0e955f0
Екатерина Мужжухина <awesome.170693@yandex.ru>
2023-12-04 23:05:37 +0300

    исправила двойную запись в бд

--- backend/api/chat_consumers.py
+++ backend/api/chat_consumers.py
@@ -1,24 +1,24 @@
-import base64
 import json
-import secrets
-from datetime import datetime
 
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
-from django.core.files.base import ContentFile
-from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
 
-from users.models import User
-from job.models import Chat, Message
 from api.serializers.message_serializers import MessageSerializer
+from job.models import Chat, Message
 
 
 class ChatConsumer(WebsocketConsumer):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(args, kwargs)
+        self.sender = None
+        self.message_id = None
+
     def connect(self):
         if 'user' not in self.scope:
             # Если пользователь не авторизован, закрываем соединение
             self.close()
+
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = f"chat_{self.room_name}"
 
@@ -37,7 +37,6 @@ class ChatConsumer(WebsocketConsumer):
             serializer = MessageSerializer(instance=message)
             self.send(text_data=json.dumps(serializer.data))
 
-
     def disconnect(self, close_code):
         # Leave room group
         async_to_sync(self.channel_layer.group_discard)(
@@ -49,6 +48,15 @@ class ChatConsumer(WebsocketConsumer):
         # parse the json data into dictionary object
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
+        chat = Chat.objects.get(id=int(self.room_name))
+        self.sender = self.scope["user"]     
+
+        message_create = Message.objects.create(
+                sender=self.sender,
+                text=message,
+                chat=chat,
+        )
+        self.message_id = message_create.id
 
         # Send message to room group
         async_to_sync(self.channel_layer.group_send)(
@@ -58,16 +66,22 @@ class ChatConsumer(WebsocketConsumer):
 
     # Receive message from room group
     def chat_message(self, event):
-        message = event["message"]
+        # message = event["message"]
+        # sender = self.scope["user"]
+        if self.message_id:
+            print("sender")
+            message = Message.objects.filter(id=self.message_id)
+            serializer = MessageSerializer(instance=message[0])
 
-        chat = Chat.objects.get(id=int(self.room_name))
-        sender = self.scope['user']
+            self.send(text_data=json.dumps(serializer.data))
+            self.sender = None
+            self.message_id = None
 
-        message = Message.objects.create(
-            sender=sender,
-            text=message,
-            chat=chat,
-        )
-        serializer = MessageSerializer(instance=message)
-        # Send message to WebSocket
-        self.send(text_data=json.dumps(serializer.data))
+        else:
+            print("receiver")
+            chat = Chat.objects.get(id=int(self.room_name))
+            # Получаем последнее сообщение и отправляем его на отображение
+            last_message = Message.objects.filter(chat=chat).order_by('-pub_date').first()
+            if last_message:
+                serializer = MessageSerializer(instance=last_message)
+                self.send(text_data=json.dumps(serializer.data))
