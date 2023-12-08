f99bb38fa1b82c45a19719e695b1287fd1170c92
Екатерина Мужжухина <awesome.170693@yandex.ru>
2023-12-09 00:25:10 +0300

    add send_message

--- backend/api/views/message_views.py
+++ backend/api/views/message_views.py
@@ -4,33 +4,36 @@ from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
-from api.permissions import IsInitiatorOrReceiverMessagePermission
 from api.serializers.message_serializers import MessageSerializer
 from job.models import Chat
+from users.models import User
 
 
 class MessageViewSet(viewsets.ModelViewSet):
     """"
-    Класс MessageViewSet для работы с сообщениями чатов.
+    Класс MessageViewSet для отправки сообщений.
 
     """
 
     serializer_class = MessageSerializer
-    http_method_names = ['get', 'post']
-    permission_classes = [
-        IsAuthenticated,
-        IsInitiatorOrReceiverMessagePermission
-    ]
+    http_method_names = ['post']
+    permission_classes = [IsAuthenticated]
 
     def get_chat(self):
         user = self.request.user
-        return get_object_or_404(
-            Chat.objects.filter(Q(initiator=user) | Q(receiver=user)),
-            pk=self.kwargs.get('chat_id'),
-        )
-
-    def get_queryset(self):
-        return self.get_chat().messages.all()
+        receiver_id = self.request.data.get('receiver')
+        chat = Chat.objects.filter(
+            Q(initiator__id=user.id, receiver__id=receiver_id)
+            | Q(initiator__id=receiver_id, receiver__id=user.id)
+        ).first()
+        if not chat:
+            chat = Chat.objects.create(
+                initiator=user,
+                receiver=get_object_or_404(
+                    User.objects.filter(pk=receiver_id)
+                )
+            )
+        return chat
 
     def perform_create(self, serializer):
         serializer.save(
