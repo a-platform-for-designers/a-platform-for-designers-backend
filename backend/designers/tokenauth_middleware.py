70abbc4711ea177bdff01862b88ae6f43bcea9f5
Екатерина Мужжухина <awesome.170693@yandex.ru>
2023-12-21 23:42:24 +0300

    pass token for websocket in cookies

--- backend/designers/tokenauth_middleware.py
+++ backend/designers/tokenauth_middleware.py
@@ -1,11 +1,10 @@
 from channels.db import database_sync_to_async
-from channels.middleware import BaseMiddleware
 from django.contrib.auth.models import AnonymousUser
+from rest_framework.authtoken.models import Token
 
 
 @database_sync_to_async
 def get_user(token_key):
-    from rest_framework.authtoken.models import Token
     try:
         token = Token.objects.get(key=token_key)
         return token.user
@@ -13,15 +12,19 @@ def get_user(token_key):
         return AnonymousUser()
 
 
-class TokenAuthMiddleware(BaseMiddleware):
-
+class TokenAuthMiddleware:
     def __init__(self, inner):
         self.inner = inner
 
     async def __call__(self, scope, receive, send):
-        headers = dict(scope['headers'])
-        if b'authorization' in headers:
-            token_name, token_key = headers[b'authorization'].decode().split()
-            if token_name == 'Token':
-                scope['user'] = await get_user(token_key)
-        return await super().__call__(scope, receive, send)
+        headers = scope.get("headers", [])
+        for name, value in headers:
+            if name.decode("utf-8") == "cookie":
+                token = next((
+                    v.split("=")[1] for v in value.decode(
+                        "utf-8"
+                    ).split("; ") if v.startswith("token=")
+                ), None)
+                if token:
+                    scope["user"] = await get_user(token)
+        return await self.inner(scope, receive, send)
