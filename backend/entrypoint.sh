4c063e38957003a886934f358e3da596fb1eb03e
Dave-YP <115189185+Dave-YP@users.noreply.github.com>
2023-12-22 17:28:45 +0300

    +

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,11 +2,11 @@
 
 ./wait-for-it.sh db:5432 --timeout=30
 
-# python manage.py makemigrations users
-# python manage.py makemigrations job
-# python manage.py migrate users
-# python manage.py migrate job
+python manage.py makemigrations users
+python manage.py makemigrations job
+python manage.py migrate users --fake
+python manage.py migrate job --fake
 # python manage.py load_data
-# python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
 
 exec gunicorn designers.wsgi:application --bind 0:8000
