baf72ed70726397d03c5470c29535097a13b5983
Dave-YP <115189185+Dave-YP@users.noreply.github.com>
2023-12-24 15:41:28 +0300

    +

--- backend/designers/tokenauth_middleware.py
+++ backend/designers/tokenauth_middleware.py
@@ -1,7 +1,7 @@
 from channels.db import database_sync_to_async
 from django.contrib.auth.models import AnonymousUser
 from rest_framework.authtoken.models import Token
-
+from urllib.parse import parse_qs
 
 @database_sync_to_async
 def get_user(token_key):
@@ -11,20 +11,24 @@ def get_user(token_key):
     except Token.DoesNotExist:
         return AnonymousUser()
 
-
 class TokenAuthMiddleware:
     def __init__(self, inner):
         self.inner = inner
 
     async def __call__(self, scope, receive, send):
-        headers = scope.get("headers", [])
-        for name, value in headers:
-            if name.decode("utf-8") == "cookie":
-                token = next((
-                    v.split("=")[1] for v in value.decode(
-                        "utf-8"
-                    ).split("; ") if v.startswith("token=")
-                ), None)
-                if token:
-                    scope["user"] = await get_user(token)
+        # Извлекаем параметры строки запроса из scope
+        query_string = scope.get("query_string", b"").decode("utf-8")
+        query_params = parse_qs(query_string)
+
+        # Проверяем, есть ли токен среди параметров строки запроса
+        token_key = query_params.get("token")
+        if token_key:
+            # Извлекаем первый токен из списка параметров
+            token = token_key[0]
+            # Получаем пользователя по токену и обновляем scope
+            scope["user"] = await get_user(token)
+        else:
+            # Если токен не найден, устанавливаем AnonymousUser
+            scope["user"] = AnonymousUser()
+
         return await self.inner(scope, receive, send)
