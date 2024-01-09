894f4814aa7262ceb4693f8bb2f300ee4b40a62b
Dave-YP <115189185+Dave-YP@users.noreply.github.com>
2024-01-09 14:11:18 +0300

    +

--- backend/api/views/user_views.py
+++ backend/api/views/user_views.py
@@ -214,7 +214,19 @@ class UserProfileViewSet(viewsets.ModelViewSet):
         }
     )
     def list(self, request, *args, **kwargs):
-        return super().list(request, *args, **kwargs)
+        queryset = self.filter_queryset(self.get_queryset())
+
+        if not queryset.exists():
+            # Если запрос фильтрации не находит соответствующих пользователей
+            return Response([], status=status.HTTP_200_OK)
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
     @extend_schema(
         summary="Получение информации о конкретном пользователе",
