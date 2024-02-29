f62a31667a9612c5c2d2542d7525da3b4fad4f9a
Dave-YP <115189185+Dave-YP@users.noreply.github.com>
2024-02-29 15:53:14 +0300

    +

--- backend/designers/settings.py
+++ backend/designers/settings.py
@@ -218,6 +218,7 @@ EMAIL_HOST = os.getenv('EMAIL_HOST')
 EMAIL_PORT = int(os.getenv('EMAIL_PORT'))
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
 EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL')
+
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
